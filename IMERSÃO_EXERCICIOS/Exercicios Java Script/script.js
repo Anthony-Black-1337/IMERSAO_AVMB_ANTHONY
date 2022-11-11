9afe8bcc6e82166a4531814dafdaf5b650f63732
Anthony-1337 <84737002+Anthony-Black-1337@users.noreply.github.com>
2022-11-11 00:10:16 -0300

    Add files via upload

---
+++ IMERSÃO_EXERCICIOS/Exercicios Java Script/script.js
@@ -0,0 +1,27 @@
+
+function func1()
+{
+ document.getElementById('cor').style.background = 'red';
+}
+function func2()
+{
+ document.getElementById('cor').style.background = 'green';
+}
+function func3()
+{
+ document.getElementById('cor').style.background = 'blue';
+}
+function func4()
+{
+ document.getElementById('cor').style.background = 'yellow';
+}
+function func5()
+{
+ if(document.getElementById('p1').style.display =='none')
+ {
+     document.getElementById('p1').style.display ='inline-block'
+ }  
+ else{
+     document.getElementById('p1').style.display ='none'
+ }
+}
